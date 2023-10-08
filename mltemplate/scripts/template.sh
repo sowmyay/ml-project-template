746158cbc1f3284060d9c11fbbba16e678383886
sowmyay <sowmyayellapragada@gmail.com>
2023-10-08 19:19:38 +0200

    bugfix

--- mltemplate/scripts/template.sh
+++ mltemplate/scripts/template.sh
@@ -8,7 +8,8 @@ cp -r "$path_to_template" "$1"
 mv "$1/$template_name" "$1/$1"
 
 # sed definition differs between linux and macos systems
-if [[ "$OSTYPE" == "darwin"* ]]; then
+echo "$OSTYPE"
+if [[ "$OSTYPE" == "linux"* || "$OSTYPE" == "darwin"* ]]; then
   find "$1" -iname "*.py" -type f -exec sed -i '' "s/$template_name/$1/gi" {} \;
   sed -i '' "s/$template_name/$1/g" "$1"/Makefile "$1"/README.md
 else
