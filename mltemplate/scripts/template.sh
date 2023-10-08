376a4695fec9d0438c339a497d057dccfbaa6adf
Sowmya Yellapragada <sowmyayellapragada@gmail.com>
2023-10-08 19:46:44 +0200

    V1.0.4 (#9)
    * Bugfix for linux systems

--- mltemplate/scripts/template.sh
+++ mltemplate/scripts/template.sh
@@ -4,8 +4,16 @@ echo "Creating ML template for $1"
 path_to_template="$2"
 template_name="$3"
 
-cp -r "$path_to_template" "$1"
+cp -r "$path_to_template" .
+mv "$template_name" "$1"
 mv "$1/$template_name" "$1/$1"
-find "$1" -iname "*.py" -type f -exec sed -i '' "s/$template_name/$1/gi" {} \;
-sed -i '' "s/$template_name/$1/g" "$1"/Makefile "$1"/README.md
+
+# sed definition differs between linux and macos systems: https://stackoverflow.com/a/43453459
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find "$1" -iname "*.py" -type f -exec sed -i '' "s/$template_name/$1/gi" {} \;
+  sed -i '' "s/$template_name/$1/g" "$1"/Makefile "$1"/README.md
+else
+  find "$1" -iname "*.py" -type f -exec sed -i "s/$template_name/$1/gi" {} \;
+  sed -i "s/$template_name/$1/g" "$1"/Makefile "$1"/README.md
+fi
 echo "Voila!"
