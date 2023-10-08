d55f3d642b0c5acbcc200268bed14e6f4c25047a
sowmyay <sowmyayellapragada@gmail.com>
2023-10-08 16:33:08 +0200

    Improvements to generalize template creationg

--- mltemplate/scripts/pytorch-template.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Creating ML template for $1"
-path_to_template="$2"
-cp -r "$path_to_template" "$1"
-mv "$1"/pytorch_template "$1/$1"
-find "$1" -iname "*.py" -type f -exec sed -i '' "s/pytorch_template/$1/gi" {} \;
-sed -i '' "s/pytorch_template/$1/g" "$1"/Makefile "$1"/README.md
-echo "Voila!"
