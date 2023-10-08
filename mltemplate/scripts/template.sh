d2c36691ca9435636834e28986591732cbc09767
sowmyay <sowmyayellapragada@gmail.com>
2023-10-08 19:40:01 +0200

    more

--- mltemplate/scripts/template.sh
+++ mltemplate/scripts/template.sh
@@ -4,12 +4,12 @@ echo "Creating ML template for $1"
 path_to_template="$2"
 template_name="$3"
 
-cp -r "$path_to_template" "$1"
+cp -r "$path_to_template" .
+mv "$template_name" "$1"
 mv "$1/$template_name" "$1/$1"
 
-# sed definition differs between linux and macos systems
-echo "$OSTYPE"
-if [[ "$OSTYPE" == "linux"* || "$OSTYPE" == "darwin"* ]]; then
+# sed definition differs between linux and macos systems: https://stackoverflow.com/a/43453459
+if [[ "$OSTYPE" == "darwin"* ]]; then
   find "$1" -iname "*.py" -type f -exec sed -i '' "s/$template_name/$1/gi" {} \;
   sed -i '' "s/$template_name/$1/g" "$1"/Makefile "$1"/README.md
 else
