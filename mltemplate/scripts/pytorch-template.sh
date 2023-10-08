7d86386e81e01efa34b491e6164a63eb8ec10ceb
sowmyay <sowmyayellapragada@gmail.com>
2023-10-08 14:05:05 +0200

    Add voila

--- mltemplate/scripts/pytorch-template.sh
+++ mltemplate/scripts/pytorch-template.sh
@@ -5,3 +5,4 @@ cp -r pytorch_template "$1"
 mv "$1"/pytorch_template "$1/$1"
 find "$1" -iname "*.py" -type f -exec sed -i '' "s/pytorch_template/$1/gi" {} \;
 sed -i '' "s/pytorch_template/$1/g" "$1"/Makefile "$1"/README.md
+echo "Voila!"
